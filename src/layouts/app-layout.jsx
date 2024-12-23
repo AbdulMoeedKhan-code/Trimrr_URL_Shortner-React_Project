a198027854396f4803af4614f5e5ff43cb5bf4e2
AbdulMoeedKhan-code <abdulmoeedkhan209@gmail.com>
2024-12-23 14:00:15 +0500

    first commit

---
+++ src/layouts/app-layout.jsx
@@ -0,0 +1,18 @@
+import Header from "@/components/header";
+import {Outlet} from "react-router-dom";
+
+const AppLayout = () => {
+  return (
+    <div>
+      <main className="min-h-screen container">
+        <Header />
+        <Outlet />
+      </main>
+      <div className="p-10 text-center bg-gray-800 mt-10">
+        Made with 💗 by <a href="#" className="text-blue-400">Abdul Moeed Khan</a>
+      </div>
+    </div>
+  );
+};
+
+export default AppLayout;
